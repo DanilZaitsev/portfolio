ce0546f481c7b2bda73fd9449f3f413cb2a15cdb
DanilZaitsev <zdanil86@gmail.com>
2022-12-20 15:08:59 -0800

    Init

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,5 @@ echo $1
 cd /home/ubuntu/portfolio
 docker-compose stop
 docker-compose rm -f
-docker images|grep portfolio|grep -v $1|awk -F'       ' '{print "docker rmi "$1":"$2}'|bash
+docker images -a | grep "portfolio"| awk '{print $3}'| xargs docker rmi
 docker-compose up -d
