6ffec1bb84b134a26c6425680039976f7eee4aa6
DanilZaitsev <zdanil86@gmail.com>
2022-12-20 14:40:01 -0800

    Init

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,5 @@
 cd portfolio
 docker-compose stop
 docker-compose rm
-docker images|grep portfolio|grep -v ${{ env.RELEASE_VERSION }}|awk -F'       ' '{print "docker rmi "$1":"$2}'|bash
+docker images|grep portfolio|grep -v $1|awk -F'       ' '{print "docker rmi "$1":"$2}'|bash
 docker-compose -d up
