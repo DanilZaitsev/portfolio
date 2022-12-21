ba3d3966ddea5e96a2c1c0b5a0fa030bf8014662
DanilZaitsev <zdanil86@gmail.com>
2022-12-20 16:04:42 -0800

    Init

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,5 @@ cd /home/ubuntu/portfolio
 docker-compose stop
 docker-compose rm -f
 docker images -a | grep "portfolio"| awk '{print $3}'| xargs docker rmi
+docker-compose build --no-cache
 docker-compose up -d
